959894d277d6e20603de18a1971b538cac746982
anett7 <anette.kurucz@gmail.com>
2023-02-26 16:09:16 +0000

    Submission1 -assignment 1
    •  Use the window.alert() and document.write() methods.
    •  Assign string values to two variables and concatenate the variables together.
    •  Assign a color to at least one variable using the fontcolor() method.
    •  Write an expression.
    •  Add comments in your JavaScript explaining your code.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+window.alert("Welcome on my page!");//Use the window.alert()
+var Family = "Stein", Mum = "Anett", Dad = "Balazs";
+var Family = Family.fontcolor("red");//Assign a color to at least one variable using the fontcolor() method.
+document.write(Family);//use document.write() methods.
+var Mum = Mum.fontcolor("#0080ff");
+document.write(Mum);
+var Dad = Dad.fontcolor("#00264d");
+document.write(Dad);
+
+
+document.write(3+3);//Write an expression.
+
+
+var A="\"Be who you are and say what you feel,"
++" because those who mind don't matter and those who matter don't mind.\""
+var B="-Dr Seuss"//Assign string values to two variables and concatenate the variables together.
+document.write(A+B)
