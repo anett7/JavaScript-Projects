591cb8f3d3e88eb23a3d080be5b03a067ab7006e
anett7 <anette.kurucz@gmail.com>
2023-04-01 18:52:49 +0100

    assignment submission 9.
    correction to clear countdown after alert

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -3,10 +3,11 @@ function countdown() {
     function tick() {
         seconds =seconds -1;
         timer.innerHTML =seconds;
-        setTimeout(tick, 1000);
+        var time=setTimeout(tick, 1000);
         if(seconds == -1) {
             alert("Time's up!");
-        
+        clearTimeout(time)
+        timer.innerHTML =""
         }
     }
     tick();
