8f93914394eb16b91ac9d9c3ee8e2886bbfd8c57
anett7 <anette.kurucz@gmail.com>
2023-04-05 20:26:17 +0100

    Assignment Submission # 10
    Write a function that use a while loop
    Write a function that use a for loop
    Write a function that use an array
    Create an object using the “let” keyword

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,3 +1,4 @@
+
 //counting loop
 
 function Call_Loop() {
@@ -24,12 +25,43 @@ function for_Loop() {
 document.getElementById ("List_of_Instruments").innerHTML = Content;
 }
 
+//array
 function cat_pics() {
     var Cat_Picture = [];
     Cat_Picture[0] = "sleeping";
     Cat_Picture[1] = "playing";
     Cat_Picture[2] = "eating";
     Cat_Picture[3] = "purring";
-    document.getElementById("Cat").innerHTML = "In this picture, the cat is" + Cat_Picture[2] + ".";
+    document.getElementById("Cat").innerHTML = "In this picture, the cat is " + Cat_Picture[2] + ".";
 
 }
+
+function constant_function() {
+    const Musical_Instrument = { type: "guitar ", brand:"Fender", color:"black"};
+    Musical_Instrument.color = "blue";
+    Musical_Instrument.price = "$900";
+    document.getElementById("Constant").innerHTML = "The cost of the " + Musical_Instrument.type + "was " + Musical_Instrument.price;
+    }
+    
+       
+          //return statement
+    function myFunction(name) {
+      return "Hello " + name;
+    }
+    document.getElementById("demo").innerHTML = myFunction("John");
+
+    //object property function
+
+    function object_property_function() {
+    let car = {
+        make:"Dodge",
+        model: "Viper",
+        year: "2021 ",
+        color: "red ",
+        description: function() {
+            return "The car is a " + this.year + this.color + this.make + this.model;
+         }
+     }
+        document.getElementById("car_object").innerHTML = car.description();
+
+    }
